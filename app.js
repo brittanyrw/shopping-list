2df6d12e369de5eb3dcf502cf59b6818a3e3102d
Brittany Walker <breneewalker@gmail.com>
2017-01-24 22:19:20 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// .submit(), preventDefault(), toggleClass(), and closest().
+//enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+//check and uncheck items on the list by clicking the "Check" button
+//permanently remove items from the list
