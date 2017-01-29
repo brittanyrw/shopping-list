86180e460cdbef802727a9a2c91ba506bab5895f
Brittany Walker <breneewalker@gmail.com>
2017-01-29 10:45:27 -0500

    update

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
-// .submit(), preventDefault(), toggleClass(), and closest().
-//enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-//check and uncheck items on the list by clicking the "Check" button
-//permanently remove items from the list
-
 $(document).ready(function() {
 
-	$("[type="submit"]").click(function(event) { 
+	//add item to list when 'add item' button is clicked
+	$(".submit").click(function(event) { 
 
 		event.preventDefault();
-		
-		var userInput = $("#shopping-list-entry").val();
-		console.log(userInput);
+		addItem();
 	});
 
+	// add item to list when enter is pressed
+	$("#shopping-list-entry").keydown(function(event) {
+  	if ( event.which == 13 ) {
+     	event.preventDefault();
+     	addItem();
+  		}
+  });
+
+	// delete item from list when delete button is clicked
+	$(".shopping-item-delete").click(function(event){
+
+		deleteItem();
+	});
+
+	// cross out item when check button is clicked
+	$("shopping-item-toggle").click(function(event){
+		checkItem();
+	});	
+
 });
+
+function addItem() {
+	var newItem = $('#shopping-list-entry').val();
+    $('.shopping-list').val();
+    $('.shopping-list').append('<li>' +
+      '<span class="shopping-item js-shopping-item">' + newItem + '</span>' +
+      '<div class="shopping-item-controls">' +
+        '<button class="js-shopping-item-toggle">' +
+          '<span class="button-label">check</span>' +
+        '</button>' +
+        '<button class="js-shopping-item-delete">' +
+          '<span class="button-label">delete</span>' +
+        '</button>' +
+      '</div>' +
+    '</li>');
+}
+
+function deleteItem() {
+	$(this).closest("li").remove();
+}
+
+function checkItem () {
+	$(this).siblings(".shopping-item").toggleClass("shopping-item__checked");
+}
+
+
